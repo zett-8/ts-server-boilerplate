67bf88c6a26cb3f8467ee73cc7d43b3c0b24012b
zett-8 <kame0008@gmail.com>
2024-07-22 19:37:22 +0200

    use helmet

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express'
+import helmet from "helmet";
 
 export const app = express()
 
 app.use(express.json())
+app.use(helmet())
 
 app.get('/', async (_, res) => {
     return res.status(200).json({ data: 'success' })
